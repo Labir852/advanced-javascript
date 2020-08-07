365d8d8891bddcf544f41fbe4fa32130b6d40e6b
Tanvir Anjum Labir <33982784+Labir852@users.noreply.github.com>
2020-08-07 16:47:11 +0600

    done

---
+++ object-property.js
@@ -0,0 +1,12 @@
+const students =[
+    {id:21,name: "omor sunny"},
+    {id:31,name: "Manna"},
+    {id:41,name: "Moyuri"},
+    {id:71,name: "Dipjol"},
+];
+const names = students.map(s=>s.name);
+const ids = students.map(s=>s.id);
+const bigger = students.filter(s=>s.id>40);
+const biggerOne = students.find(s=>s.id>40);
+
+console.log(biggerOne);
